92e69eaa72b643105eb96d673fe80a090e6bb25f
muhsinata <muhsinatadurak@gmail.com>
2022-08-30 11:26:47 +0300

    feat: make start button run timer

--- pomodoro-app.py
+++ pomodoro-app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,8 +11,19 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 
+def start_timer():
+    count_down(10)
+
+
 def count_down(count):
-    canvas.itemconfig(timer_text, text=count)
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -30,9 +42,7 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-count_down(5)
-
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
