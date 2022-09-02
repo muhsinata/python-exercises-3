dad7d767b672006e461c5820e20fb3d841716f97
muhsinata <muhsinatadurak@gmail.com>
2022-09-02 17:39:06 +0300

    feat: add new starting code

--- indexerror-handling/main.py
+++ indexerror-handling/main.py
@@ -1,6 +1,6 @@
-fruits = ["Apple", "Pear", "Orange"]
+""" fruits = ["Apple", "Pear", "Orange"]
 
-#TODO: Catch the exception and make sure the code runs without crashing.
+
 def make_pie(index):
     try:
         fruit = fruits[index]
@@ -10,4 +10,22 @@ def make_pie(index):
         print(fruit + " pie")
 
 
-make_pie(2)
+make_pie(2) """
+
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2}, 
+    {'Likes': 13, 'Comments': 2, 'Shares': 1}, 
+    {'Likes': 33, 'Comments': 8, 'Shares': 3}, 
+    {'Comments': 4, 'Shares': 2}, 
+    {'Comments': 1, 'Shares': 1}, 
+    {'Likes': 19, 'Comments': 3}
+]
+
+total_likes = 0
+
+for post in facebook_posts:
+    total_likes = total_likes + post['Likes']
+
+
+print(total_likes)
